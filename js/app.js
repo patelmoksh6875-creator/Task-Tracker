74af4c3ba8face17f429c105fab2c31158802bde
patelmoksh6875-creator <patel.moksh.6875@gmail.com>
2026-07-13 21:10:51 -0500

    Delete js directory

--- js/app.js
+++
@@ -1,7 +0,0 @@
-import { UIManager } from './ui.js';
-import { initThemeEngine } from './utils.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    initThemeEngine();
-    UIManager.init();
-});
